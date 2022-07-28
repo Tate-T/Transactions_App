295aee7c3b014548e6962a937be49833ac9f6c64
Tate-T <tatjana.tarasovych@gmail.com>
2022-07-28 16:36:57 +0300

    try inline styles

--- src/components/ImportExportBtns/ImportExportBtns.tsx
+++ src/components/ImportExportBtns/ImportExportBtns.tsx
@@ -2,12 +2,16 @@ import s from './ImportExportBtns.module.css';
 
 const ImportExportBtns: React.FC  = () => {
 
-    return (
-        <div className={s.btns}>
+    return 
+       ( <div className={s.btns}>
             <a href="#" download className={s.btn}>Import</a>
             <a href="#" download className={s.btn}>Export</a>
-        </div>
-    )
+        </div>)
+    //     <div style={{display: flex }}>
+    //     <a href="#" download style={{height: '30px', width: '100px', background-color: 'aliceblue', box-shadow: '2px 2px', border-radius: '3px'}}>Import</a>
+    //     <a href="#" download style={{height: '30px', width: '100px', background-color: 'aliceblue', box-shadow: '2px 2px', border-radius: '3px'}}>Export</a>
+    // </div>
+    
 }
 
 export  {ImportExportBtns};
