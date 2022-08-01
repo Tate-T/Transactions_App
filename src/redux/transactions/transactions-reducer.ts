434495dad6dbe3eebbb8bb05fde9017c8221b074
Tate-T <tatjana.tarasovych@gmail.com>
2022-08-01 11:48:43 +0300

    reducers

--- src/redux/transactions/transactions-reducer.ts
+++ src/redux/transactions/transactions-reducer.ts
@@ -1,5 +1,50 @@
- const transactionsReducer = () => {
-    console.log('0')
-}
+import { createReducer } from "@reduxjs/toolkit";
+import shortid from 'shortid';
+import { addTransactionApiRequest, addTransactionSuccess, addTransactionError, addToFilterState } from "./transactions-actions";
+import { getTransactions, deleteTransaction } from './transactions-types';
 
-export default transactionsReducer
+const initialState = [
+    { id: shortid.generate(), name: 'Products', status: 'pending', type: 'refill', amount: 100, action: false },
+    { id: shortid.generate(), name: 'Goods', status: 'completed', type: 'withdrawal', amount: 100, action: false },
+    { id: shortid.generate(), name: 'Services', status: 'chanceled', type: 'refill', amount: 100, action: false },
+    { id: shortid.generate(), name: 'Things', status: 'completed', type: 'withdrawal', amount: 100, action: false },
+];
+
+export const transactionsReducer = createReducer(initialState, {
+    [addTransactionSuccess]: (state, { payload }) => [...state, payload],
+    [getTransactions.fulfilled]: (_, { payload }) => payload,
+    [deleteTransaction.fulfilled]: (state, { payload }) =>
+        state.filter(transaction => transaction.id !== payload)
+});
+
+export const isLoadingReducer = createReducer(false, {
+    [addTransactionApiRequest]: () => true,
+    [addTransactionSuccess]: () => false,
+    [addTransactionError]: () => false,
+
+    // за 2-м варіантом через async thunk:
+
+    [getTransactions.pending]: () => true,
+    [getTransactions.fulfilled]: () => false,
+    [getTransactions.rejected]: () => false,
+    [deleteTransaction.pending]: () => true,
+    [deleteTransaction.fulfilled]: () => false,
+    [deleteTransaction.rejected]: () => false,
+})
+
+export const filterReducer = createReducer('', {
+    [addToFilterState]: (_, { payload }) => payload,
+
+});
+
+const setError = (_, { payload }) => payload;
+const resetError = () => null;
+
+export const errorReducer = createReducer(null, {
+    [addTransactionError]: setError,
+    [addTransactionApiRequest]: resetError,
+    [getTransactions.rejected]: setError,
+    [getTransactions.pending]: resetError,
+    [deleteTransaction.rejected]: setError,
+    [deleteTransaction.pending]: resetError,
+});
