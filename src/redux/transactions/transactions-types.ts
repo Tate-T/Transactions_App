6ce7fe8a766d6d95c1c46c1432c26096ebbd1da2
Tate-T <tatjana.tarasovych@gmail.com>
2022-08-01 11:39:54 +0300

    operations

--- src/redux/transactions/transactions-types.ts
+++ src/redux/transactions/transactions-types.ts
@@ -1,5 +1,35 @@
-const transactionsTypes = () => {
-    console.log('0')
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { addTransactionApi, getTransactionApi, deleteTransactionApi } from "../../services/mockapi";
+import { addTransactionApiRequest, addTransactionSuccess, addTransactionError } from './transactions-actions';
+
+// спосіб 1:
+
+export const addTransaction = (transaction) => {
+    return (dispatch) => {
+        dispatch(addTransactionApiRequest());
+
+        addTransactionApi(transaction)
+            .then((data) => dispatch(addTransactionSuccess(data)))
+            .catch((err) => dispatch(addTransactionError(err.message)));
+    }
 }
 
-export default transactionsTypes
+// інший варіант через async thunk:
+
+export const getTransactions = createAsyncThunk('transactions/get/:id', async (id, thunkApi) => {
+    try {
+        const transactions = await getTransactionApi(id);
+        return transactions;
+    } catch (error) {
+        return thunkApi.rejectWithValue(error.message);
+    }
+});
+
+export const deleteTransaction = createAsyncThunk('transactions/delete/:id', async (id, thunkApi) => {
+    try {
+        await deleteTransactionApi(id);
+        return id;
+    } catch (error) {
+        return thunkApi.rejectWithValue(error.message);
+    }
+});
